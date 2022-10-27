e75df49fef64f923b7efd37dd148632c5a462a46
yxa.kravchenko@yandex.ru <yxa.kravchenko@yandex.ru>
2022-10-27 23:35:21 +0300

    Добавьте файлы проекта.

---
+++ Lesson 11/Linear equation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11
+{
+    struct Linear_equation
+    {
+        double k;
+        double b;
+
+        public Linear_equation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            double x = -b / k;
+            if (k == 0 && b == 0)
+                return "Всегда будет 0";
+
+            double x1 = -b / k;
+            if (k == 0 && b != 0)
+                return "Корней нет";
+            
+            double x2 = -b / k;
+            return $"{x2}";
+         
+        }
+    }
+
+}
+
---
+++ Lesson 11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое значение");
+            double k = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите второе значение");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Результат: ");
+            Linear_equation linear = new Linear_equation(k,b);
+
+            Console.WriteLine(linear.Root());
+            Console.ReadKey();  
+
+        }
+    }
+}
